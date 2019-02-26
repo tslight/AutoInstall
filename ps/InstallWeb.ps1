23161e4fbb787be3ec8392e69f3aa7c24849758b
tslight <tobyslight@gmail.com>
2019-02-26 21:11:16 +0000

    it's been a while & I've learnt a lot!

--- ps/InstallWeb.ps1
+++ ps/InstallWeb.ps1
@@ -1,25 +1,45 @@
-$source = 'W:\Scripts\Toby\pkgps\installers'
+$InstallerPath = 'W:\Scripts\Toby\pkgps\installers'
 
-If (!(Test-Path -Path $source -PathType Container))
-{
-    New-Item -Path $source -ItemType Directory | Out-Null
+If (!(Test-Path -Path $InstallerPath -PathType Container)) {
+    New-Item -Path $InstallerPath -ItemType Directory | Out-Null
 }
 
-$packages = @(
-    @{title='Chrome';url='http://dl.google.com/chrome/install/375.126/chrome_installer.exe';Arguments=' /silent /install';Destination=$source},
-    @{title='Reader';url='http://ardownload.adobe.com/pub/adobe/reader/win/AcrobatDC/1801120035/AcroRdrDC1801120035_en_US.exe';Arguments=' /msi EULA_ACCEPT=YES /qn /quiet';Destination=$source}
-    @{title='VLC';url='https://get.videolan.org/vlc/3.0.0/win64/vlc-3.0.0-win64.exe';Arguments=' /L=1033 /S';Destination=$source}
+$ChromeUrl = "http://dl.google.com/chrome/install"
+$ChromeVersion = "375.126/chrome_installer.exe"
+$ReaderUrl = "http://ardownload.adobe.com/pub/adobe/reader/win/AcrobatDC"
+$ReaderVersion = "1801120035/AcroRdrDC1801120035_en_US.exe"
+$VlcUrl = "https://get.videolan.org/vlc"
+$VlcVersion = "3.0.0/win64/vlc-3.0.0-win64.exe"
+
+$Packages = @(
+    @{
+	Title		= 'Chrome'
+	Url		= "$ChromeUrl/$ChromeVersion"
+	Arguments	= ' /silent /install'
+	Destination	= $InstallerPath
+    },
+    @{
+	Title		= 'Reader'
+	Url		= "$ReaderUrl/$ReaderVersion"
+	Arguments	= ' /msi EULA_ACCEPT=YES /qn /quiet'
+	Destination	= $InstallerPath
+    },
+    @{
+	Title		= 'VLC'
+	Url		= "$VlcUrl/$VlcVersion"
+	Arguments	= ' /L=1033 /S'
+	Destination	= $InstallerPath
+    }
 )
 
 Write-Host -fore Yellow "`nDownloading Packages ..."
-foreach ($package in $packages)
-{
-    $packageName = $package.title
-    $fileName = Split-Path $package.url -Leaf
+
+foreach ($package in $packages) {
+    $packageName = $package.Title
+    $fileName = Split-Path $package.Version -Leaf
     $destinationPath = $package.Destination + "\" + $fileName
 
-    If (!(Test-Path -Path $destinationPath -PathType Leaf))
-    {
+    if (!(Test-Path -Path $destinationPath -PathType Leaf)) {
 	Write-Host "Downloading $packageName ..."
 	$webClient = New-Object System.Net.WebClient
 	$webClient.DownloadFile($package.url,$destinationPath)
@@ -30,8 +50,8 @@ foreach ($package in $packages)
 }
 
 Write-Host -fore Yellow "`nInstalling Packages ..."
-foreach ($package in $packages)
-{
+
+foreach ($package in $packages) {
     $packageName = $package.title
     $fileName = Split-Path $package.url -Leaf
     $destinationPath = $package.Destination + "\" + $fileName
